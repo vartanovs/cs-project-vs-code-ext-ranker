4ce5452132a0bd20ca81078feeb5ee542e69cb0c
Serge Vartanov <vartanov.s@gmail.com>
2018-09-20 01:31:27 -0700

    Password Authentication and Security

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
+const bcrypt = require('bcrypt');
 const express = require('express');
 const bodyParser = require('body-parser');
 const Promise = require('bluebird');
 const sqlite = require('sqlite');
+const path = require('path');
 
 const app = express();
 const dbPromise = sqlite.open('./database.db', { Promise });
 
 app.set('view engine', 'ejs');
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
-
-console.log(dbPromise);
+const saltRounds = 10;
 
 // Route for '/' to enable index
 app.get('/', async (req, res, next) => {
@@ -27,6 +28,11 @@ app.get('/', async (req, res, next) => {
   }
 });
 
+// Route for registration form
+app.get('/form.html', (req, res) => {
+  res.sendFile(path.join(__dirname + '/form.html'));
+});
+
 // Route for login.html to confirm login success
 app.post('/login.html', urlencodedParser, async (req, res, next) => {
   // Cache email address and password
@@ -38,7 +44,7 @@ app.post('/login.html', urlencodedParser, async (req, res, next) => {
     const userArray = await Promise.all(db.all('SELECT * FROM user_table'));
     // Loop through accounts to confirm if user/pass combination is available
     userArray.forEach((user) => {
-      if (user.email === logInAcct && user.hash_pass === logInPass) {
+      if (user.email === logInAcct && bcrypt.compareSync(logInPass, user.hash_pass)) {
         // If combination found, render extList page
         res.render('extList', { user });
       }
@@ -56,11 +62,13 @@ app.post('/register.html', urlencodedParser, async (req, res, next) => {
   const lastName = req.body.lastname;
   const email = req.body.email;
   const pass = req.body.pass;
+  const hashPass = bcrypt.hashSync(pass, saltRounds);
   try {
     const db = await dbPromise;
     // Query all user accounts from database, cache in userArray
-    const userInput = await Promise.all(db.all(`INSERT INTO user_table (first_name, last_name, email, hash_pass) VALUES ('${firstName}', '${lastName}', '${email}', '${pass}');`));
-    const user = await Promise.all(db.all(`SELECT * FROM user_table WHERE email='${email}';`));
+    await Promise.all(db.all(`INSERT INTO user_table (first_name, last_name, email, hash_pass) VALUES ('${firstName}', '${lastName}', '${email}', '${hashPass}');`));
+    let user = await Promise.all(db.all(`SELECT * FROM user_table WHERE email='${email}';`));
+    user = user[0]
     console.log(user);
     res.render('extList', { user });
   } catch (err) {
