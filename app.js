d74803a64686bbd02ead78dacce45b1aa5a09722
Serge Vartanov <vartanov.s@gmail.com>
2018-09-20 00:37:39 -0700

    User Login Password MVP Authentication

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const Promise = require('bluebird');
 const sqlite = require('sqlite');
 
@@ -6,6 +7,7 @@ const app = express();
 const dbPromise = sqlite.open('./database.db', { Promise });
 
 app.set('view engine', 'ejs');
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 console.log(dbPromise);
 
@@ -20,6 +22,47 @@ app.get('/', async (req, res, next) => {
     ]);
     console.log('test', user, vscExt);
     res.render('index', { user, vscExt });
+  } catch (err) {
+    next(err);
+  }
+});
+
+// Route for login.html to confirm login success
+app.post('/login.html', urlencodedParser, async (req, res, next) => {
+  // Cache email address and password
+  const logInAcct = req.body.account;
+  const logInPass = req.body.pass;
+  try {
+    const db = await dbPromise;
+    // Query all user accounts from database, cache in userArray
+    const userArray = await Promise.all(db.all('SELECT * FROM user_table'));
+    // Loop through accounts to confirm if user/pass combination is available
+    userArray.forEach((user) => {
+      if (user.email === logInAcct && user.hash_pass === logInPass) {
+        // If combination found, render extList page
+        res.render('extList', { user });
+      }
+    });
+    res.render('index', { userArray });
+  } catch (err) {
+    next(err);
+  }
+});
+
+// Route for login.html to confirm login success
+app.post('/register.html', urlencodedParser, async (req, res, next) => {
+  // Cache all inputs
+  const firstName = req.body.firstname;
+  const lastName = req.body.lastname;
+  const email = req.body.email;
+  const pass = req.body.pass;
+  try {
+    const db = await dbPromise;
+    // Query all user accounts from database, cache in userArray
+    const userInput = await Promise.all(db.all(`INSERT INTO user_table (first_name, last_name, email, hash_pass) VALUES ('${firstName}', '${lastName}', '${email}', '${pass}');`));
+    const user = await Promise.all(db.all(`SELECT * FROM user_table WHERE email='${email}';`));
+    console.log(user);
+    res.render('extList', { user });
   } catch (err) {
     next(err);
   }
